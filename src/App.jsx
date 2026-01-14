045ed10727fbdf91db3b0d931ee4217d41f93666
Tsimmuaj <tyang4204@gmail.com>
2026-01-14 13:17:06 -0600

    Started on Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Header } from "./Header"
+import { Section } from "./Section"
 
 
 function App() {
@@ -6,6 +7,8 @@ function App() {
     <div>
       <Header/>
       <p>Hellow Atlas</p>
+      <Section/>
+      <Section/>
     </div>
   );
 }
